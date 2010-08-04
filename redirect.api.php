e98f507c361f2548df34cf7b929c1ee84d95109a
Dave Reid <dave@davereid.net>
2010-08-04 07:24:26 +0000

    by Dave Reid: Fixed redirect_load_by_source() to do the heavy lifting of checking query and language by using uasort callback _redirect_uasort(). by Dave Reid: Fixed redirect_redirect() to only accept a redirect array as its parameter. by Dave Reid: Added and improved documentation.

---
+++ redirect.api.php
@@ -0,0 +1,40 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Documentation for the redirect module API.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+function hook_redirect_load(array &$redirects) {
+
+}
+
+function hook_redirect_presave(array &$redirect) {
+
+}
+
+function hook_redirect_insert(array &$redirect) {
+
+}
+
+function hook_redirect_update(array &$redirect) {
+
+}
+
+function hook_redirect_delete(array &$redirect) {
+
+}
+
+function hook_redirect_redirect_alter(array &$redirect) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
