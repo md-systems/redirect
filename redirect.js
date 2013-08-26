0b7b8dc2d58cb277874d87c91c45f0a361e148f7
Dave Reid <dave@davereid.net>
2013-08-26 17:32:29 -0500

    Issue #2049151 by mauritsl, justanothermark: Fixed vertical summary with number of redirects was incorrect.

--- redirect.js
+++ redirect.js
@@ -4,11 +4,11 @@
 Drupal.behaviors.redirectFieldsetSummaries = {
   attach: function (context) {
     $('fieldset.redirect-list', context).drupalSetSummary(function (context) {
-      if ($('table.redirect-list tbody td.empty', context).size()) {
+      if ($('table.redirect-list tbody td.empty', context).length) {
         return Drupal.t('No redirects');
       }
       else {
-        var redirects = $('table.redirect-list tbody tr').size();
+        var redirects = $('table.redirect-list tbody tr', context).length;
         return Drupal.formatPlural(redirects, '1 redirect', '@count redirects');
       }
     });
