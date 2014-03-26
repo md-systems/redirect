211b0014bc330b2ea187d1f3d80d7206db4a6177
Martin Micuch <martin.micuch@gmail.com>
2014-03-26 10:16:57 +0100

    Port redirect_can_redirect().

--- lib/Drupal/redirect/EventSubscriber/RedirectRequestSubscriber.php
+++ lib/Drupal/redirect/EventSubscriber/RedirectRequestSubscriber.php
@@ -9,10 +9,13 @@ namespace Drupal\redirect\EventSubscriber;
 
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\KeyValueStore\StateInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Routing\UrlGenerator;
 use Drupal\redirect\RedirectRepository;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -41,6 +44,16 @@ class RedirectRequestSubscriber implements EventSubscriberInterface {
   protected $config;
 
   /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Drupal\Core\KeyValueStore\StateInterface
+   */
+  protected $state;
+
+  /**
    * Constructs a \Drupal\redirect\EventSubscriber\RedirectRequestSubscriber object.
    *
    * @param \Drupal\Core\Routing\UrlGenerator $url_generator
@@ -51,12 +64,15 @@ class RedirectRequestSubscriber implements EventSubscriberInterface {
    *   The language manager service.
    * @param \Drupal\Core\Config\ConfigFactoryInterface
    *   The config.
+   * @param \Drupal\Core\KeyValueStore\StateInterface
+   *   The state.
    */
-  public function __construct(UrlGenerator $url_generator, RedirectRepository $redirect_repository, LanguageManagerInterface $language_manager, ConfigFactoryInterface $config) {
+  public function __construct(UrlGenerator $url_generator, RedirectRepository $redirect_repository, LanguageManagerInterface $language_manager, ConfigFactoryInterface $config, StateInterface $state) {
     $this->urlGenerator = $url_generator;
     $this->redirectRepository = $redirect_repository;
     $this->languageManager = $language_manager;
     $this->config = $config->get('redirect.settings');
+    $this->state = $state;
   }
 
   /**
@@ -68,11 +84,9 @@ class RedirectRequestSubscriber implements EventSubscriberInterface {
   public function onKernelRequestCheckRedirect(GetResponseEvent $event) {
     $request = $event->getRequest();
 
-    // @todo - this needs to become part of the subscriber class, otherwise
-    //   unit tests wont work.
-//    if (!redirect_can_redirect()) {
-//      return;
-//    }
+    if (!$this->canRedirect($request)) {
+      return;
+    }
 
     // Get URL info and process it to be used for hash generation.
     parse_str($request->getQueryString(), $request_query);
@@ -112,6 +126,43 @@ class RedirectRequestSubscriber implements EventSubscriberInterface {
       $response = new RedirectResponse($url, $redirect->getStatusCode(), array('X-Redirect-ID' => $redirect->id()));
       $event->setResponse($response);
     }
+  }
+
+  /**
+   * Determines if redirect may be performed.
+   *
+   * @param Request $request
+   *   The current request object.
+   *
+   * @return bool
+   *   TRUE if redirect may be performed.
+   */
+  public function canRedirect(Request $request) {
+    $can_redirect = TRUE;
+
+    $route = $request->attributes->get(RouteObjectInterface::ROUTE_OBJECT);
+    if ($route) {
+      $is_admin = (bool) $route->getOption('_admin_route');
+    }
+
+    if ($request->getScriptName() != 'index.php') {
+      // Do not redirect if the root script is not /index.php.
+      $can_redirect = FALSE;
+    }
+    elseif (!$request->isMethod('GET')) {
+      // Do not redirect if this is other than GET request.
+      $can_redirect = FALSE;
+    }
+    elseif ($this->state->get('system.maintenance_mode') || defined('MAINTENANCE_MODE')) {
+      // Do not redirect in offline or maintenance mode.
+      $can_redirect = FALSE;
+    }
+    elseif (!$this->config->get('global_admin_paths') && !empty($is_admin)) {
+      // Do not redirect on admin paths.
+      $can_redirect = FALSE;
+    }
+
+    return $can_redirect;
   }
 
   /**
--- lib/Drupal/redirect/Tests/RedirectLogicTest.php
+++ lib/Drupal/redirect/Tests/RedirectLogicTest.php
@@ -12,6 +12,9 @@ use Drupal\redirect\EventSubscriber\RedirectRequestSubscriber;
 use Drupal\redirect\EventSubscriber\RedirectTerminateSubscriber;
 use Drupal\Tests\UnitTestCase;
 use PHPUnit_Framework_MockObject_MockObject;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
@@ -198,6 +201,130 @@ class RedirectLogicTest extends UnitTestCase {
   }
 
   /**
+   * Tests the can redirect check.
+   */
+  public function testCanRedirect() {
+    $url_generator = $this->getMockBuilder('Drupal\Core\Routing\UrlGenerator')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $state = $this->getMockBuilder('Drupal\Core\KeyValueStore\StateInterface')
+      ->getMock();
+    $state->expects($this->any())
+      ->method('get')
+      ->with('system.maintenance_mode')
+      ->will($this->returnValue(FALSE));
+
+    $subscriber = new RedirectRequestSubscriber(
+      $url_generator,
+      $this->getRedirectRepositoryStub('findMatchingRedirect', NULL),
+      $this->getLanguageManagerStub(),
+      $this->getConfigFactoryStub(array('redirect.settings' => array('global_admin_paths' => FALSE))),
+      $state
+    );
+
+    // All fine - we can redirect.
+    $request = $this->getRequestStub('index.php', 'GET');
+    $this->assertTrue($subscriber->canRedirect($request), 'Can redirect');
+
+    // The script name is not index.php.
+    $request = $this->getRequestStub('not_index.php', 'GET');
+    $this->assertFalse($subscriber->canRedirect($request), 'Cannot redirect script name not index.php');
+
+    // The request method is not GET.
+    $request = $this->getRequestStub('index.php', 'POST');
+    $this->assertFalse($subscriber->canRedirect($request), 'Cannot redirect other than GET method');
+
+    // Maintenance mode is on.
+    $state = $this->getMockBuilder('Drupal\Core\KeyValueStore\StateInterface')
+      ->getMock();
+    $state->expects($this->any())
+      ->method('get')
+      ->with('system.maintenance_mode')
+      ->will($this->returnValue(TRUE));
+
+    $subscriber = new RedirectRequestSubscriber(
+      $url_generator,
+      $this->getRedirectRepositoryStub('findMatchingRedirect', NULL),
+      $this->getLanguageManagerStub(),
+      $this->getConfigFactoryStub(array('redirect.settings' => array('global_admin_paths' => FALSE))),
+      $state
+    );
+
+    $request = $this->getRequestStub('index.php', 'GET');
+    $this->assertFalse($subscriber->canRedirect($request), 'Cannot redirect if maintenance mode is on');
+
+    // We are at a admin path.
+    $state = $this->getMockBuilder('Drupal\Core\KeyValueStore\StateInterface')
+      ->getMock();
+    $state->expects($this->any())
+      ->method('get')
+      ->with('system.maintenance_mode')
+      ->will($this->returnValue(FALSE));
+
+    $subscriber = new RedirectRequestSubscriber(
+      $url_generator,
+      $this->getRedirectRepositoryStub('findMatchingRedirect', NULL),
+      $this->getLanguageManagerStub(),
+      $this->getConfigFactoryStub(array('redirect.settings' => array('global_admin_paths' => FALSE))),
+      $state
+    );
+
+    $route = $this->getMockBuilder('Symfony\Component\Routing\Route')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $route->expects($this->any())
+      ->method('getOption')
+      ->with('_admin_route')
+      ->will($this->returnValue('system.admin_config_search'));
+
+    $request = $this->getRequestStub('index.php', 'GET',
+      array(RouteObjectInterface::ROUTE_OBJECT => $route));
+    $this->assertFalse($subscriber->canRedirect($request), 'Cannot redirect if we are requesting a admin path');
+
+    // We are at admin path with global_admin_paths set to TRUE.
+    $subscriber = new RedirectRequestSubscriber(
+      $url_generator,
+      $this->getRedirectRepositoryStub('findMatchingRedirect', NULL),
+      $this->getLanguageManagerStub(),
+      $this->getConfigFactoryStub(array('redirect.settings' => array('global_admin_paths' => TRUE))),
+      $state
+    );
+
+    $request = $this->getRequestStub('index.php', 'GET',
+      array(RouteObjectInterface::ROUTE_OBJECT => $route));
+    $this->assertTrue($subscriber->canRedirect($request), 'Can redirect a admin with global_admin_paths set to TRUE');
+  }
+
+  /**
+   * Gets request mock object.
+   *
+   * @param $script_name
+   *   The result of the getScriptName() method.
+   * @param $method
+   *   The request method.
+   * @param array $attributes
+   *   Attributes to be passed into request->attributes.
+   *
+   * @return PHPUnit_Framework_MockObject_MockObject
+   */
+  protected function getRequestStub($script_name, $method, array $attributes = array()) {
+    $request = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $request->expects($this->any())
+      ->method('getScriptName')
+      ->will($this->returnValue($script_name));
+    $request->expects($this->any())
+      ->method('isMethod')
+      ->with($this->anything())
+      ->will($this->returnValue($method == 'GET'));
+    $request->attributes = new ParameterBag($attributes);
+
+    return $request;
+  }
+
+  /**
    * Instantiates the subscriber and runs onKernelRequestCheckRedirect()
    *
    * @param $url_generator
@@ -210,11 +337,16 @@ class RedirectLogicTest extends UnitTestCase {
    *   Flag if to retain the query through the redirect.
    */
   protected function assertOnKernelRequestCheckRedirect($url_generator, $redirect, $request_query, $retain_query) {
+
+    $state = $this->getMockBuilder('Drupal\Core\KeyValueStore\StateInterface')
+      ->getMock();
+
     $subscriber = new RedirectRequestSubscriber(
       $url_generator,
       $this->getRedirectRepositoryStub('findMatchingRedirect', $redirect),
       $this->getLanguageManagerStub(),
-      $this->getConfigFactoryStub(array('redirect.settings' => array('passthrough_querystring' => $retain_query)))
+      $this->getConfigFactoryStub(array('redirect.settings' => array('passthrough_querystring' => $retain_query))),
+      $state
     );
 
     // Run the main redirect method.
@@ -323,6 +455,15 @@ class RedirectLogicTest extends UnitTestCase {
     $request->expects($this->any())
       ->method('getPathInfo')
       ->will($this->returnValue($path_info));
+    $request->expects($this->any())
+      ->method('getScriptName')
+      ->will($this->returnValue('index.php'));
+    $request->expects($this->any())
+      ->method('isMethod')
+      ->with('GET')
+      ->will($this->returnValue(TRUE));
+
+    $request->attributes = new ParameterBag();
 
     $http_kernel = $this->getMockBuilder('\Symfony\Component\HttpKernel\HttpKernelInterface')
       ->getMock();
