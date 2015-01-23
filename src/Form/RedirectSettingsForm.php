bca6f303f4e2a5ac58ad20eef9f872c41d5ade09
Sascha Grossenbacher <saschagros@gmail.com>
2015-01-23 18:07:54 +0100

    Update for immutable config changes

--- src/Form/RedirectSettingsForm.php
+++ src/Form/RedirectSettingsForm.php
@@ -18,28 +18,34 @@ class RedirectSettingsForm extends ConfigFormBase {
   public function getFormId() {
     return 'redirect_settings_form';
   }
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['redirect.settings'];
+  }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $redirect_settings = \Drupal::config('redirect.settings')->get();
+    $config = $this->config('redirect.settings');
     $form['redirect_auto_redirect'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Automatically create redirects when URL aliases are changed.'),
-      '#default_value' => $redirect_settings['auto_redirect'],
+      '#default_value' => $config->get('auto_redirect'),
       '#disabled' => !\Drupal::moduleHandler()->moduleExists('path'),
     );
     $form['redirect_passthrough_querystring'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Retain query string through redirect.'),
-      '#default_value' => $redirect_settings['passthrough_querystring'],
+      '#default_value' => $config->get('passthrough_querystring'),
       '#description' => $this->t('For example, given a redirect from %source to %redirect, if a user visits %sourcequery they would be redirected to %redirectquery. The query strings in the redirection will always take precedence over the current query string.', array('%source' => 'source-path', '%redirect' => 'node?a=apples', '%sourcequery' => 'source-path?a=alligators&b=bananas', '%redirectquery' => 'node?a=apples&b=bananas')),
     );
     $form['redirect_warning'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Display a warning message to users when they are redirected.'),
-      '#default_value' => $redirect_settings['warning'],
+      '#default_value' => $config->get('warning'),
       '#access' => FALSE,
     );
     $form['redirect_default_status_code'] = array(
@@ -47,7 +53,7 @@ class RedirectSettingsForm extends ConfigFormBase {
       '#title' => $this->t('Default redirect status'),
       '#description' => $this->t('You can find more information about HTTP redirect status codes at <a href="@status-codes">@status-codes</a>.', array('@status-codes' => 'http://en.wikipedia.org/wiki/List_of_HTTP_status_codes#3xx_Redirection')),
       '#options' => redirect_status_code_options(),
-      '#default_value' => $redirect_settings['default_status_code'],
+      '#default_value' => $config->get('default_status_code'),
     );
     // @todo - enable once the feature is available.
     // $cache_enabled = \Drupal::config('system.performance')->get('cache.page.use_internal');
@@ -63,12 +69,12 @@ class RedirectSettingsForm extends ConfigFormBase {
     $form['redirect_purge_inactive'] = array(
       '#type' => 'select',
       '#title' => $this->t('Delete redirects that have not been accessed for'),
-      '#default_value' => $redirect_settings['purge_inactive'],
+      '#default_value' => $config->get('purge_inactive'),
       '#options' => array_map(array(\Drupal::service('date.formatter'), 'formatInterval'), array_combine($options, $options)),
       '#empty_option' => $this->t('Never (do not discard)'),
       '#empty_value' => 0,
       '#description' => $this->t('Only redirects managaged by the redirect module itself will be deleted. Redirects managed by other modules will be left alone.'),
-      '#disabled' => $redirect_settings['page_cache'] && !$invoke_hooks,
+      '#disabled' => $config->get('page_cache') && !$invoke_hooks,
     );
 
     $form['globals'] = array(
@@ -79,29 +85,29 @@ class RedirectSettingsForm extends ConfigFormBase {
     $form['globals']['redirect_global_home'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Redirect from paths like index.php and /node to the root directory.'),
-      '#default_value' => $redirect_settings['global_home'],
+      '#default_value' => $config->get('global_home'),
     );
     $form['globals']['redirect_global_clean'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Redirect from non-clean URLs to clean URLs.'),
-      '#default_value' => $redirect_settings['global_clean'],
+      '#default_value' => $config->get('global_clean'),
       // @todo - does still apply? See https://drupal.org/node/1659580
       //'#disabled' => !variable_get('clean_url', 0),
     );
     $form['globals']['redirect_global_canonical'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Redirect from non-canonical URLs to the canonical URLs.'),
-      '#default_value' => $redirect_settings['global_canonical'],
+      '#default_value' => $config->get('global_canonical'),
     );
     $form['globals']['redirect_global_deslash'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Remove trailing slashes from paths.'),
-      '#default_value' => $redirect_settings['global_deslash'],
+      '#default_value' => $config->get('global_deslash'),
     );
     $form['globals']['redirect_global_admin_paths'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Allow redirections on admin paths.'),
-      '#default_value' => $redirect_settings['global_admin_paths'],
+      '#default_value' => $config->get('global_admin_paths'),
     );
 
     return parent::buildForm($form, $form_state);
