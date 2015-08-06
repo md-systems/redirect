c59d4313ba08a67b523970d25bec4e32bca1510b
Lukas Schneider <lukas.schneider.1990@outlook.com>
2015-08-06 09:10:42 +0200

    removed unnecessary commands, moved linking to after script, moved matrix above env

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -5,11 +5,3 @@ set -e $DRUPAL_TI_DEBUG
 # Ensure the right Drupal version is installed.
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
-
-# Add needed dependencies.
-cd "$DRUPAL_TI_DRUPAL_DIR"
-ln -sf $(pwd) subdir
-
-# These variables come from environments/drupal-*.sh
-mkdir -p "$DRUPAL_TI_MODULES_PATH"
-cd "$DRUPAL_TI_MODULES_PATH"
